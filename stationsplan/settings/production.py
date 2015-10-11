76711d9db8857c9e1a548cc2a64184840d5b8bae
Johannes Ammon <j.ammon@dr-ammon.de>
2015-10-11 17:26:28 +0200

    Logging added

--- stationsplan/settings/production.py
+++ stationsplan/settings/production.py
@@ -45,3 +45,23 @@ TEMPLATES = [{
         ],
     },
 }]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/www/stationsplan/priv/logs/debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
