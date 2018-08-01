3ab4166d2093cd7683d3e1ae99d4f922df912faf
Johannes Ammon <j.ammon@dr-ammon.de>
2018-08-01 23:02:45 +0200

    Problem mit Debug Toolbar

--- stationsplan/settings/dev.py
+++ stationsplan/settings/dev.py
@@ -17,12 +17,12 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-MIDDLEWARE = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-) + MIDDLEWARE
+# INSTALLED_APPS += (
+#     'debug_toolbar',
+# )
+# MIDDLEWARE = (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ) + MIDDLEWARE
 INTERNAL_IPS = ('127.0.0.1', 'localhost')
 
 DJANGO_TEMPLATES['DIRS'] = []
