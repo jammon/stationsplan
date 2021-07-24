41e25cb46c4694e2cd72e8125801425bb63ce37a
Johannes Ammon <j.ammon@dr-ammon.de>
2021-07-24 11:58:14 +0200

    Fix migration

--- sp_app/migrations/0054_auto_20210526_1039.py
+++ sp_app/migrations/0054_auto_20210526_1039.py
@@ -4,22 +4,25 @@ from django.db import migrations
 
 
 PERMISSIONS = (
-    ('Editors', 'is_editor'),
-    ('Department admins', 'is_dep_lead'),
-    ('Company Admin', 'is_company_admin'),
+    ('Editors', 'is_editor', 'is editor for a department'),
+    ('Department admins', 'is_dep_lead', 'is leader of a department'),
+    ('Company Admin', 'is_company_admin', 'is admin of the company'),
 )
 
 
 def migrate_permissions(apps, schema_editor):
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
-    for g, p in PERMISSIONS:
-        try:
-            group = Group.objects.get(name=g)
-            permission = Permission.objects.get(codename=p)
-            group.permissions.add(permission)
-        except Group.DoesNotExist:
-            pass
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    Editor = apps.get_model('sp_app', 'Editor')
+
+    content_type = ContentType.objects.get_for_model(Editor)
+    for g, p, name in PERMISSIONS:
+        group, _ = Group.objects.get_or_create(name=g)
+        permission, _ = Permission.objects.get_or_create(
+            codename=p,
+            defaults={'name': name, 'content_type': content_type})
+        group.permissions.add(permission)
 
 
 class Migration(migrations.Migration):
