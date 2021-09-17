c78405c90125eadc1fb1b509d58d93ea50399e47
Johannes Ammon <j.ammon@dr-ammon.de>
2021-09-17 12:32:27 +0200

    CallTallies waren weg, reformatiert

--- stationsplan/urls.py
+++ stationsplan/urls.py
@@ -24,11 +24,15 @@ from sp_app.admin import config_site
 
 urlpatterns = [
     path("", sp_views.home, name="home"),
+    #
     # SPA
     re_path("plan/(?P<month>[0-9]+)?/?$", sp_views.plan, name="plan"),
-    re_path(r"^dienste(/(?P<month>[0-9]+))?/?$", sp_views.plan, name="dienste"),
+    re_path(
+        r"^dienste(/(?P<month>[0-9]+))?/?$", sp_views.plan, name="dienste"
+    ),
     re_path(r"^tag(/(?P<day>[0-9]+))?/?$", sp_views.plan, name="tag"),
     path("zuordnung", sp_views.plan, name="functions"),
+    #
     # Ajax
     path("change_function", sp_ajax.change_function, name="change_function"),
     path("changes", sp_ajax.changes, name="changes"),
@@ -39,35 +43,54 @@ urlpatterns = [
     ),
     path("set_approved", sp_ajax.change_approved, name="set_approved"),
     re_path(r"^updates/([0-9]+)/?$", sp_ajax.updates, name="updates"),
+    #
     # Administrators
     path("personen", sp_views.personen_funktionen, name="persons"),
-    path("person/add/", sp_views.PersonCreateView.as_view(), name="person-add"),
-    path("person/<int:pk>/", sp_views.PersonUpdateView.as_view(), name="person-update"),
+    path(
+        "person/add/", sp_views.PersonCreateView.as_view(), name="person-add"
+    ),
+    path(
+        "person/<int:pk>/",
+        sp_views.PersonUpdateView.as_view(),
+        name="person-update",
+    ),
     path("funktion/add/", sp_views.WardCreateView.as_view(), name="ward-add"),
-    path("funktion/<int:pk>/", sp_views.WardUpdateView.as_view(), name="ward-update"),
+    path(
+        "funktion/<int:pk>/",
+        sp_views.WardUpdateView.as_view(),
+        name="ward-update",
+    ),
     path(
         "different_day/<str:action>/<int:ward>/<str:day_id>",
         sp_ajax.differentday,
         name="different-day",
     ),
+    #
     # Other
     path(
-        "tests", TemplateView.as_view(template_name="sp_app/tests.html"), name="tests"
+        "tests",
+        TemplateView.as_view(template_name="sp_app/tests.html"),
+        name="tests",
     ),
     path(
         "password_change",
         auth_views.PasswordChangeView.as_view(
-            template_name="registration/password_change.html", success_url="/plan"
+            template_name="registration/password_change.html",
+            success_url="/plan",
         ),
         name="password_change",
     ),
     path("admin/", admin.site.urls),
     path("config/", config_site.urls),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path(
+        "logout", auth_views.LogoutView.as_view(next_page="/"), name="logout"
+    ),
     path(
         "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        TemplateView.as_view(
+            template_name="robots.txt", content_type="text/plain"
+        ),
     ),
     path("/", include("django.contrib.auth.urls")),
 ]
